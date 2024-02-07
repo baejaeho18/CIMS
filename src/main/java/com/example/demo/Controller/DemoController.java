5209350200b63be73b7119c3079d3d85ea2995f0
jaeho <human@handong.ac.kr>
2024-02-07 13:54:59 +0900

    [Demo] web demo

---
+++ src/main/java/com/example/demo/Controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.demo.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    @GetMapping("/maplist")
+    public String maplist(Model model){
+        model.addAttribute("data", "hello!!");
+        return "maplist";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model){
+        model.addAttribute("data", "dashboard!!");
+        return "dashboard";
+    }
+}
