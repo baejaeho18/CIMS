90b33bad5f851584f5981a47cdfc0c4516b9906a
jaeho <human@handong.ac.kr>
2024-02-25 21:27:23 +0900

    refac: remove demoController and old htmls

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/login", "/signup").permitAll()
-                        .requestMatchers("/home", "/cctv/**").authenticated()
+                        .requestMatchers("/home", "/cctv/**", "/dashboard").authenticated()
                         .requestMatchers("/memberList", "/delete", "/promote").hasRole("ADMIN")
                 )
                 .formLogin((formIn) -> formIn
