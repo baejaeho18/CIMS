0e1a952bee6c8c684d601b44aa3fe87a0af90d80
jaeho <human@handong.ac.kr>
2024-02-29 23:24:04 +0900

    refac: optimize Mappers

--- src/main/java/com/example/demo/repository/MemberMapper.java
+++ src/main/java/com/example/demo/repository/MemberMapper.java
@@ -14,14 +14,14 @@ public interface MemberMapper {
     Member insertMember(Member member);
 
     // 6. 회원 가입시 중복 id 검사를 위해 selectById    // selectById
-    Member selectMemberById(String id);
+    Boolean isMemberExistsById(String id);
 
     // 5. member list 받으려 selectAll               // selectAll
     List<Member> selectAll();
 
     // 4. member 삭제시 delete                       // deleteMember
-    void deleteMember(Member member);
+    void deleteMemberById(String id);
 
     // 3. member 승급시 update                       // updateMember
-    int promptMember(Member member);
+    int promptMemberById(String id);
 }
